3685edef2b2fad27b172892249435982a9700631
Jody Wilson <jody.wilson@proton.me>
2024-07-23 18:52:37 -0700

    fixed error in cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -29,7 +29,7 @@ cacheSolve <- function(x, ...) {
     inv = x$getinverse()
     if( !is.null( inv ) ){ return(inv) }
     mat <- x$get()
-    inv <- solve( mat )
+    inv <- solve( mat, ... )
     x$setinverse( inv )
     inv
 }
